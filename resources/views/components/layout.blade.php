09b519f8a3bbf504ac72b979d61835a48d8af34d
Rikoreifat <rikoreifat@gmail.com>
2025-01-05 18:50:35 +0700

    Login modified

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,7 @@
             <div class="col-sm-auto bg-light sticky-top">
                 <div class="d-flex flex-sm-column flex-row flex-nowrap bg-light align-items-center sticky-top">
                     <a href="/" class="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
-                        <i class=""><img src="{{URL::asset('img/logo.png')}}" style="height: 100px" alt=""></i>
+                        <i class=""><img src="{{URL::asset('img/logo.png')}}" style="height: 100px" alt=""><p class="fw-bold">Lagoa Express.</p></i>
                     </a>
                     <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center align-items-center">
                         <li class="nav-item">
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,10 @@ Route::get('/', function () {
 Route::get('/home', function (){
     return view('home');
 });
+
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
