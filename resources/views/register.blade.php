af5503c620eac72ba07d4baf2589883a5bd11b9f
Ahmad Fauzi Ramadhan <ramadhanahmadfauzi3@gmail.com>
2025-01-05 18:36:32 +0700

    Added Login and Register page

---
+++ resources/views/register.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Login Lagoa</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+
+<body class="d-flex justify-content-center">
+    <div class="d-flex flex-column justify-content-center align-items-center" style="background: #00000000;width: 456px;height: 660px;color: var(--bs-emphasis-color);"><img src="{{URL::asset('img/logo.png')}}" class="me-auto" width="100" height="80" style="height: 250px;width: 250px;">
+        <h3 class="me-auto">Baru dateng? bikin akun dulu sini!</h3>
+        <h6 class="me-auto" style="color: rgb(89,91,95);">Biar bisa lanjut menggunakan Lagoa Express</h6>
+    </div>
+    <div class="d-flex flex-column justify-content-center align-items-center" style="background: rgba(248,247,255,0);width: 371px;">
+        <form class="justify-content-center" style="margin: 5px;padding: 0px;"><label class="form-label" style="color: var(--bs-emphasis-color);">Nama Lengkap</label><input class="form-control" type="text"><label class="form-label" style="color: var(--bs-emphasis-color);padding-top: 20px;">Email</label><input class="form-control" type="text"><label class="form-label" style="color: var(--bs-emphasis-color);padding-top: 20px;">Password</label><input class="form-control" type="text" style="margin-top: 0px;margin-bottom: 25px;"><input class="btn btn-primary" type="submit" style="margin-right: 0px;margin-bottom: 0px;margin-left: 0px;width: 270.0469px;margin-top: 20px;color: var(--bs-emphasis-color);"></form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
