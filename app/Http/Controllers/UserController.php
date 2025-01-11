d52aab202cacdd22ebf22b7b059e143355b5003f
Rizal Anditama <rizalanditama2105@gmail.com>
2025-01-11 20:34:44 +0700

    moved home file to root and adding migration and seeder

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function login(){
+    public function login()
+    {
         if (request()->isMethod('post')) {
             $email = request('email');
             $password = request('password');
             $user = User::where('email', $email)->first();
             if ($user && $user->password == $password) {
-                session(['user' => $user]);
+                Auth::login($user);
                 return redirect()->route('home');
             }
 
@@ -23,19 +25,22 @@ class UserController extends Controller
         return view('login');
     }
 
-    public function register(){
+    public function register()
+    {
         if (request()->isMethod('post')) {
             $data = request()->validate([
                 'name' => 'required',
                 'email' => 'required|email',
-                'password' => 'required|min:6',
+                'password' => 'required|min:8|confirmed',
             ]);
 
             $data['password'] = bcrypt($data['password']);
 
             $user = User::create($data);
 
-            return redirect()->route('login');
+            Auth::login($user);
+
+            return redirect()->route('home');
         }
 
         return view('register');
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,43 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
-  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
-  <div class="login-container">
-    <div class="left-section">
-      <img src="{{ asset('img/logo.png') }}" alt="Lagoa Express Logo" class="logo">
-      <h1>Langsung masuk aja!</h1>
-      <p>Biar nanti bisa lanjut make Sofyan EXPRESS</p>
-    </div>
-    <div class="right-section">
-      <form class="login-form" action="{{ route('login') }}" method="POST">
-        @csrf
-        <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
+    <div class="login-container">
+        <div class="left-section">
+            <img src="{{ asset('img/logo.png') }}" alt="Lagoa Express Logo" class="logo">
+            <h1>Langsung masuk aja!</h1>
+            <p>Biar bisa lanjut make LAGOA EXPRESS</p>
+        </div>
+        <div class="right-section">
+            <form class="login-form" action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
 
-        <button type="submit">Lanjut</button>
-        
-        <hr>
+                <button type="submit">Lanjut</button>
 
-        
-        <div class="d-flex justify-content-center">
-          Belum punya akun?&nbsp;<a href="{{ route('register') }}" class="text-decoration-none">register</a>
-      </div>
-      </form>
+                <hr>
+
+
+                <div class="d-flex justify-content-center">
+                    Belum punya akun?&nbsp;<a href="{{ route('register') }}" class="text-decoration-none">register</a>
+                </div>
+            </form>
+        </div>
     </div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
