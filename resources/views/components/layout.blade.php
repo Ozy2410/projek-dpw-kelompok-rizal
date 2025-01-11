549c904b791ebbf2d8511cc90727deb94d7c1a72
Rizal Anditama <rizalanditama2105@gmail.com>
2025-01-11 22:51:19 +0700

    added home page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{ $title ?? 'Lagoa Express' }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -21,10 +21,12 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-auto bg-light sticky-top">
-                <div class="d-flex flex-column flex-shrink-0 bg-body-tertiary" style="width: 4.5rem;">
-                    <a href="/" class="d-block link-body-emphasis text-decoration-none mb-3"
+                <div class="d-flex flex-column flex-shrink-0 bg-body-tertiary">
+                    <a href="/"
+                        class="d-flex justify-content-center align-items-center link-body-emphasis text-decoration-none mb-3"
                         data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Lagoa Express">
-                       <img src="{{asset("img/logo lagoa express.png")}}" width="64" height="64" alt="">
+                        <img src="{{ asset('img/logo lagoa express.png') }}" width="64" height="64"
+                            alt="">
                         <span class="visually-hidden">Lagoa Express</span>
                     </a>
                     <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
@@ -32,13 +34,19 @@
                             <a href="#" class="nav-link active py-3 border-bottom rounded-0" aria-current="page"
                                 data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Home"
                                 data-bs-original-title="Home">
-                                <i class="bi bi-house-door fs-4"></i>
+                                <i class="bi bi-house-door fs-5"></i>
                             </a>
                         </li>
                         <li>
                             <a href="#" class="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip"
                                 data-bs-placement="right" aria-label="Tickets" data-bs-original-title="Tickets">
-                                <i class="bi bi-ticket-perforated fs-4"></i>
+                                <i class="bi bi-ticket-perforated fs-5"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="nav-link py-3 border-bottom rounded-0" data-bs-toggle="tooltip"
+                                data-bs-placement="right" aria-label="Login" data-bs-original-title="Login">
+                                <i class="bi bi-box-arrow-in-right fs-5"></i>
                             </a>
                         </li>
                     </ul>
@@ -46,14 +54,10 @@
                         <a href="#"
                             class="d-flex align-items-center justify-content-center p-3 link-body-emphasis text-decoration-none dropdown-toggle"
                             data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="mdo" width="42" height="42"
+                            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32"
                                 class="rounded-circle">
                         </a>
                         <ul class="dropdown-menu text-small shadow">
-                            <li><a class="dropdown-item" href="#">Profile</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
                             <li><a class="dropdown-item" href="#">Sign out</a></li>
                         </ul>
                     </div>
@@ -61,7 +65,7 @@
             </div>
             <div class="col p-0 min-vh-100">
                 <!-- content -->
-                    {{ $slot }}
+                {{ $slot }}
             </div>
         </div>
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,65 @@
-<x-layout>
-    <img src="{{ asset('img/background kereta.jpg') }}" alt="" class="w-25">
+<x-layout title="Home | Lagoa Express">
+    <style>
+        .background {
+            width: 100%;
+            height: 400px;
+            background:
+                linear-gradient(rgba(55, 114, 101, 0.5), rgba(26, 82, 82, 0.055)),
+                url('{{ asset('img/background kereta.jpg') }}') no-repeat;
+            background-blend-mode: overlay;
+        }
+    </style>
+    <div class="w-100 h-100 d-flex align-items-center background">
+        <div class="mx-5">
+            <div class="d-block">
+                <h3 class="text-white mt-5 pt-5">Hai, Kamu!</h3>
+                <p class="fs-3 text-white">Mau kemana kita?</p>
+            </div>
+            <div class="card bg-white mt-5" style="width: 80vw;">
+                <div class="card-body">
+                    {{-- <form action="" method="post"> --}}
+                    @csrf
+                    <div class="row">
+                        <div class="col-3">
+                            <label class="fw-bold text-secondary" for="asal_kota">Asal Kota</label>
+                            <div class="input-group">
+                                <i class="bi bi-train-front input-group-text"></i>
+                                <input type="text" id="asal_kota" name="asal_kota" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <label class="fw-bold text-secondary" for="kota_tujuan">Kota Tujuan</label>
+                            <div class="input-group">
+                                <i class="bi bi-train-front input-group-text"></i>
+                                <input type="text" id="kota_tujuan" name="kota_tujuan" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <label class="fw-bold text-secondary" for="tanggal_berangkat">Tanggal Berangkat</label>
+                            <div class="input-group">
+                                <i class="bi bi-calendar-event input-group-text"></i>
+                                <input type="text" id="tanggal_berangkat" name="tanggal_berangkat"
+                                    class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-2">
+                            <label class="fw-bold text-secondary" for="jumlah_penumpang">Jumlah
+                                Penumpang</label>
+                            <div class="input-group">
+                                <i class="bi bi-people input-group-text"></i>
+                                <input type="text" id="jumlah_penumpang" name="jumlah_penumpang"
+                                    class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-1">
+                            <div class="d-flex align-items-center justify-content-center">
+                                <button type="submit" class="btn btn-primary">Cari Kereta</button>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- </form> --}}
+                </div>
+            </div>
+        </div>
+    </div>
 </x-layout>
