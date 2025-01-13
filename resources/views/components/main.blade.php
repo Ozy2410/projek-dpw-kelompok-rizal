becfe5f3eeba6d98737493ea934da1627fa1c847
Aldi Afriyansyah <alabiyuafriyansyah@gmail.com>
2025-01-13 09:15:40 +0700

    fixed the tooltip

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -19,8 +19,10 @@
     {{ $slot }}
 
     <script>
-        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+        document.addEventListener('DOMContentLoaded', function () {
+            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+            const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+        });
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
